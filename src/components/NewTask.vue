efeccab16fe4333b2a4d2303f1d1b69ada258a48
joseEstudos <jose.games15@outlook.com>
2022-08-16 18:25:42 -0300

    160 - compontente nova tarefa

--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -1,25 +1,51 @@
 <template>
   <div class="new-task">
-    <input v-model="name" type="text" @keydown.enter="add"/>
-    <button @click="add">+</button>
+    <input
+      v-model="name"
+      type="text"
+      @keydown.enter="add"
+      class="form-element"
+      placeholder="Nova tarefa"
+    />
+    <button class="form-element" @click="add">+</button>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            name:''
-        }
+  data() {
+    return {
+      name: "",
+    };
+  },
+  methods: {
+    add() {
+      this.$emit("taskAdded", { name: this.name });
+      this.name = "";
     },
-    methods: {
-        add(){
-            this.$emit('taskAdded',{name:this.name})
-            this.name = ''
-        }
-    },
+  },
 };
 </script>
 
 <style>
+.new-task {
+  margin: 35px;
+}
+.form-element {
+  outline: none;
+  font-size: 2rem;
+  border: 1px solid #fff;
+  color: #fff;
+}
+input.form-element {
+  width: 500px;
+  background: #fff2;
+  border-top-left-radius: 8px;
+}
+button.form-element{
+    border-left: none;
+    background: #2196f3;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+}
 </style>
